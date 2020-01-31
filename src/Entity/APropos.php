46fb0d65f1b936923174336d0c69388eec683c4e
Adjagarail <adjagarail@gmail.com>
2020-01-31 18:23:20 +0100

    vish pour APropos

--- src/Entity/APropos.php
+++ src/Entity/APropos.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ApiResource()
+ * @Vich\Uploadable
  * @ORM\Entity(repositoryClass="App\Repository\AProposRepository")
  */
 class APropos
@@ -40,8 +43,20 @@ class APropos
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @var string
      */
     private $Postere;
+    /**
+     * @Vich\UploadableField(mapping="postere_images", fileNameProperty="Postere")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -94,6 +109,24 @@ class APropos
         $this->APropos = $APropos;
 
         return $this;
+    }
+
+    public function setImageFile(File $Postere = null)
+    {
+        $this->imageFile = $Postere;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($Postere) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
     }
 
     public function getPostere(): ?string

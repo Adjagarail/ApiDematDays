a2b6fbd25363d5b98b2053e6efbb330fb804dde6
Adjagarail <adjagarail@gmail.com>
2020-01-31 18:02:18 +0100

    vish pour Sponsor

--- src/Entity/Sponsors.php
+++ src/Entity/Sponsors.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ApiResource()
+ * @Vich\Uploadable
  * @ORM\Entity(repositoryClass="App\Repository\SponsorsRepository")
  */
 class Sponsors
@@ -25,8 +28,21 @@ class Sponsors
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @var string
      */
     private $Logo;
+
+    /**
+     * @Vich\UploadableField(mapping="sponsor_images", fileNameProperty="Logo")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -43,6 +59,24 @@ class Sponsors
         $this->NomSponsor = $NomSponsor;
 
         return $this;
+    }
+
+    public function setImageFile(File $Logo = null)
+    {
+        $this->imageFile = $Logo;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($Logo) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
     }
 
     public function getLogo(): ?string
